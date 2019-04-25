41f855c3c978ae9f09b0f2c4375482f9f5a83b33
gregcrom <greg.crom@hotmail.com>
2019-04-25 13:21:19 +0200

    test

--- controllers/userController.php
+++ controllers/userController.php
@@ -1,13 +1,28 @@
 <?php
 	class userController {
 		private $_global;
-		
+		private $_user;
+		private $_questions;
+
 		public function __construct($global) {
 			$this->_global = $global;
+			$this->_user=$this->_global['db']->getUser($_GET['username']);
+
+			$this->_questions=$this->global['db']->getQuestion($this->_user['id']);
+
+			if(empty($this->_user)) throw new Error("404");
+
+            $cleanUserName = $this->_global['fn']->clean($this->_user->getUsername());
+
+            if ($cleanUserName !== $_GET['username']) {
+                header("Location: /question/".$cleanUserName);
+                exit();
+            }
+
 			require_once (PATH_VIEWS."heads/userHead.php");
 		}
 		
 		public function run() {
-		
+            require_once (PATH_VIEWS."user.php");
 		}
 	}
